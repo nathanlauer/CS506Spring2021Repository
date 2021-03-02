55db3dc3ec5a25fab5cfac82e3fb51cf07ce2a32
Adam <astreich@bu.edu>
2021-03-01 22:07:11 -0500

    cleaning up commit

--- mapc2/parquet_to_csv.py
+++ mapc2/parquet_to_csv.py
@@ -4,5 +4,5 @@ import pandas as pd
 # pip install python-snappy
 # pip install pyarrow
 # pip install fastparquet
-df = pd.read_parquet('/Users/adamstreich/Desktop/2020-01-01_performance_fixed_tiles.parquet')
-df.to_csv('/Users/adamstreich/Desktop/CS506/CS506Spring2021Repository/mapc2/ookla-data/ookla2020Q1.csv')
+df = pd.read_parquet('/Users/adamstreich/Desktop/2020-10-01_performance_fixed_tiles.parquet')
+df.to_csv('/Users/adamstreich/Desktop/CS506/CS506Spring2021Repository/mapc2/ookla-data/ookla2020Q4.csv')
