e6a45d2f7691deeddad44a7ccb92a2f84f5159f1
Adam <astreich@bu.edu>
2021-03-01 21:39:16 -0500

    minor changes

--- mapc2/parquet_to_csv.py
+++ mapc2/parquet_to_csv.py
@@ -4,5 +4,5 @@ import pandas as pd
 # pip install python-snappy
 # pip install pyarrow
 # pip install fastparquet
-df = pd.read_parquet('/Users/adamstreich/Desktop/data506')
+df = pd.read_parquet('/Users/adamstreich/Desktop/2020-01-01_performance_fixed_tiles.parquet')
 df.to_csv('/Users/adamstreich/Desktop/data506MOD.csv')
